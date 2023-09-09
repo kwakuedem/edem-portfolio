4e933508dcb4215092275b2069b9ec6576487213
Edem <edemkwakuavorley@gmail.com>
2023-09-09 14:10:45 -0700

    first commit of my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className=' text-blue-500'>Hello! this is my portfolio</p> 
+    </>
+  )
+}
+
+export default App
